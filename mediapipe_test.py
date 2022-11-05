839de9e58f406f03737101642c2458085918d05d
s92475mark <792475mark@gmail.com>
2022-11-05 20:22:29 +0800

    鼠標建置

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -17,7 +17,12 @@ f_round = True 	#第一次跑
 # mpDraw = mp.solutions.drawing_utils
 # handLmsStyle = mpDraw.DrawingSpec(color=(0,255,0),thickness = 5 )	#設定點的參數
 # handConStyle = mpDraw.DrawingSpec(color=(255,255,255),thickness = 2 )	#設定線的參數
-
+def rat(newblack,CanvasSize,MousePose):
+	MouseLevel = np.full((newblack.shape[0],newblack.shape[1],3),(0,0,0),np.uint8)	#產生與newblack大小相同黑色的圖
+	MouseLevel = cv2.circle(MouseLevel, MousePose, 10, (255,255,255), -1)	#在這層上面點上白色鼠標
+	# cv2.imshow("MouseLevel",MouseLevel)
+	TrueCanvas = cv2.add(newblack,MouseLevel)
+	return TrueCanvas
 # 根據兩點的座標，計算角度
 def vector_2d_angle(v1, v2):
 	v1_x = v1[0]
@@ -71,48 +76,21 @@ def hand_angle(hand_):#計算五隻手指的角度函式
 def PointPprocessing(hands_Pose,hands_LR):	#分別處理左右手座標之副程式	(左手要做什麼，右手要做什麼 分別計算)
 	global frame,Main_hand,newblack
 	finger_points = []			# 記錄手指節點座標的串列
-	color=(0, 0, 225)
 	Hand_Mark_blue = (255,0,0)	#顏色藍色
 	Hand_Mark_red = (0,0,255)	#顏色紅色
+	Pose2 = [-10,-10] 
 	for i in range(len(hands_LR)):
 		Pose = (hands_Pose[hands_LR.index(hands_LR[i])])
 		Pose1 = [int(Pose.landmark[8].x * frame.shape[1]),int(Pose.landmark[8].y * frame.shape[0])]
 		###################################雙手處理###############################################
 
 		if hands_LR[i] == Main_hand:	#主手
-
-
+			Pose2 = [int(Pose.landmark[8].x * frame.shape[1]),int(Pose.landmark[8].y * frame.shape[0])]	#主手食指 給鼠標用
 			frame = cv2.circle(frame, Pose1, 10, Hand_Mark_blue, -1)
-			forefinger0 = [int(Pose.landmark[8].x * frame.shape[1]), int(Pose.landmark[8].y * frame.shape[0])]  # 食指頂端座標
-			frame = cv2.circle(frame, forefinger0, 10, (0, 0, 225), -1)
-			# 將 21 個節點換算成座標，記錄到 finger_points
-			for i in Pose.landmark:
-				x = i.x*frame.shape[1]
-				y = i.y*frame.shape[0]
-				# print(x,y)
-				finger_points.append((x,y))
-			# print(finger_points)
-			if finger_points:
-				# print(len(finger_points))
-				finger_angle = hand_angle(finger_points) # 計算手指角度，回傳長度為 5 的串列
-				# print(finger_angle)
-				# 小於 50 表示手指伸直，大於等於 50 表示手指捲縮 ,finger_angle[0]大拇指角度,finger_angle[1]食指角度,finger_angle[2]中指角度,finger_angle[3]無名指角度,finger_angle[4]小拇指角度
-				if finger_angle[1]<50 and finger_angle[0]>50 and finger_angle[2]>50 and finger_angle[3]>50 and finger_angle[4]>50 and forefinger0[0]>60 and  forefinger0[1]>60:#當食指伸直後進行畫圖
-					frame = cv2.circle(frame, (forefinger0[0], forefinger0[1]), 10, color, -1)#color是指顏色上面有預設值紅色
-					newblack = cv2.circle(newblack, (forefinger0[0], forefinger0[1]), 10, color, -1)
-				#print(forefinger0[0])食指x跟y
-				#print(forefinger0[1])
-				if finger_angle[1]<50 and finger_angle[0]<50 and finger_angle[2]<50 and finger_angle[3]<50 and finger_angle[4]<50 and forefinger0[0]<=20 and  forefinger0[1]<=20:#手指全部張開並且移到綠框時畫筆顏色變為綠
-					color=(0, 255, 0)#顏色為綠色
-				# print(color)
-				if finger_angle[1]<50 and finger_angle[0]<50 and finger_angle[2]<50 and finger_angle[3]<50 and finger_angle[4]<50 and forefinger0[0]<=20 and  20<=forefinger0[1]<=40:#手指全部張開並且食指移到紅色框格內畫筆顏色變為紅
-					color=(0, 0, 255)#顏色為紅色
-
-
-
-
+			newblack = cv2.circle(newblack, Pose1, 10, Hand_Mark_blue, -1)
 		else:	#副手運算
 			frame = cv2.circle(frame, Pose1, 10, Hand_Mark_red, -1)
+	return Pose2
 
 def HandsIdentify(imgRGB):		#副程式處理"手部座標"、"左右手順序"
 
@@ -124,28 +102,32 @@ def HandsIdentify(imgRGB):		#副程式處理"手部座標"、"左右手順序"
 		for i in range(len(results.multi_hand_landmarks)):	#001 用迴圈一次處理一隻手的座標
 			hands_Pose.append(results.multi_hand_landmarks[i])
 			hands_LR.append(hands_LR1[i].classification[0].label)
-	print(hands_LR)
+	# print(hands_LR)
 	return hands_Pose,hands_LR
 
-while(True):
-	ret,frame = cap.read()
-	if not ret:		#判定有沒有畫面存在
-		print("沒有畫面")
-		break
-	if f_round:	#判斷是不是第一次跑，是:把黑色畫布放大成跟鏡頭解析度一樣大
-		newblack = cv2.resize(newblack, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_AREA)
-		f_round = False
-	frame = cv2.flip(frame, 1)	#畫面左右翻轉
-	imgRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)	#將影像通道從BGR轉成RGB
-	hands_Pose1,hands_LR = HandsIdentify(imgRGB)	#副程式處理"手部座標"、"左右手順序"
-	results1 = PointPprocessing(hands_Pose1,hands_LR)	#分別處理左右手座標之副程式
-	cTime = time.time()
-	fps =  1/(cTime-pTime)
-	pTime = cTime
-	cv2.putText(frame, str(int(fps)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX,1,(255, 255,0), 1, cv2.LINE_AA)
-	# print(fps)
-	cv2.imshow("live",frame)
-	cv2.imshow("newblack",newblack)
-	# time.sleep(0.5)	#跑影片要記得設time.sleep，跑視訊鏡頭要記得關  我花了40分鐘在debug為甚麼我的fps不到1
-	if cv2.waitKey(1) == ord('q'):
-		break
+if __name__ == '__main__':
+	while(True):
+		ret,frame = cap.read()
+		if not ret:		#判定有沒有畫面存在
+			print("沒有畫面")
+			break
+		CanvasSize = (frame.shape[1], frame.shape[0])	#畫布大小
+		if f_round:	#判斷是不是第一次跑，是:把黑色畫布放大成跟鏡頭解析度一樣大
+			newblack = cv2.resize(newblack, CanvasSize, interpolation=cv2.INTER_AREA)
+			f_round = False
+		frame = cv2.flip(frame, 1)	#畫面左右翻轉
+		imgRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)	#將影像通道從BGR轉成RGB
+		hands_Pose1,hands_LR = HandsIdentify(imgRGB)	#副程式處理"手部座標"、"左右手順序"
+		MousePose = PointPprocessing(hands_Pose1,hands_LR)	#分別處理左右手座標之副程式
+		TrueCanvas = rat(newblack,CanvasSize,MousePose)
+
+		cTime = time.time()
+		fps =  1/(cTime-pTime)
+		pTime = cTime
+		cv2.putText(frame, str(int(fps)), (10, 40), cv2.FONT_HERSHEY_SIMPLEX,1,(255, 255,0), 1, cv2.LINE_AA)
+		# print(fps)
+		cv2.imshow("live",frame)
+		cv2.imshow("newblack",TrueCanvas)
+		# time.sleep(0.5)	#跑影片要記得設time.sleep，跑視訊鏡頭要記得關  我花了40分鐘在debug為甚麼我的fps不到1
+		if cv2.waitKey(1) == ord('q'):
+			break
